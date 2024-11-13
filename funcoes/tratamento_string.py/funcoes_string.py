d9e9cd6df5150a3728d5f5447d6ef48f3a051078
Lucas Balen Mattos <david.js2007@aluno.ifsc.edu.br>
2024-11-13 17:08:38 -0300

    Aula - 13/11

---
+++ funcoes/tratamento_string.py/funcoes_string.py
@@ -0,0 +1,22 @@
+#Questão01
+
+def calcularNumeroCaracteresString(ListaDeCaracteresString)
+    #Código a ser escrito dentro da função 
+    if(type(ListaDeCaracteresString) == str):
+        return len(ListaDeCaracteresString)
+    else: 
+        return -1 #Aqui o tamanho da string
+
+
+
+
+
+
+#Chamar a função 
+letraDaMusica = """To numa mesa com quaaatro cadeiraaaas"""
+tamanhoDaString  = calcularNumeroCaracteresString(letraDaMusica, 99.5)
+tamanhoDaString  = calcularNumeroCaracteresString(4.7)
+if(tamanhoDaString)
+
+
+print(tamanhoDaString)
