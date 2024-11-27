1b7ceb42405a5b4a7398963b9a9ec5fbceeab87d
DavidjGreensIFSC <david.js2007@aluno.ifsc.edu.br>
2024-11-27 16:55:17 -0300

    Create data_hora_no_python.py

---
+++ biblioteca.datetime/data_hora_no_python.py
@@ -0,0 +1,7 @@
+'''Pytz é uma biblioteca do Python  que permite calcular fusos horários 
+e resolver problemas com horários ambiguos no final do horário de verão.
+
+O tzdata é um 'pacote' de dados do Python que fornece dados de fuso horário
+ IANA. Ele é principalmente usado pelo módulo "zoneinfo" da biblioteca 
+ padrão. Mas também pode ser usado como fonte de dados de fuso horário.;'''
+ 
