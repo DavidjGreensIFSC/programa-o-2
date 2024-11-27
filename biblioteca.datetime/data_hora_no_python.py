5574c86c51eff8ab79f83993420e45126fb3fcdb
DavidjGreensIFSC <david.js2007@aluno.ifsc.edu.br>
2024-11-27 17:30:16 -0300

    Aula 27.11 DATETIME

--- biblioteca.datetime/data_hora_no_python.py
+++ biblioteca.datetime/data_hora_no_python.py
@@ -2,6 +2,14 @@
 e resolver problemas com horários ambiguos no final do horário de verão.
 
 O tzdata é um 'pacote' de dados do Python que fornece dados de fuso horário
- IANA. Ele é principalmente usado pelo módulo "zoneinfo" da biblioteca 
- padrão. Mas também pode ser usado como fonte de dados de fuso horário.;'''
+IANA. Ele é principalmente usado pelo módulo "zoneinfo" da biblioteca padrão.
+Mas também pode ser usado como fonte de dados de fuso horário.;'''
  
+from datetime import datetime, date, time, timedelta
+
+data_de_hoje = date.today()
+
+horario_especifico = time(hour=14, minute=30, second=35)
+
+print (data_de_hoje)
+print (horario_especifico)
